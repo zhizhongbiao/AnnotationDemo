d5755ec8896c7044d497c584473a8a7c74b569ef
zhizhongbiao <zhizhongbiao163@163.com>
2018-08-17 10:00:40 +0800

    add-增加AnnotationMemberUsage类，展示注解的成员变量的赋值 update-更新一下AnnotationTest类，展示注解定义成员变量并给成员变量默认值

--- app/src/main/java/com/yf/annotationdemo/annotation_define/AnnotationMemberUsage.java
+++ app/src/main/java/com/yf/annotationdemo/annotation_define/AnnotationMemberUsage.java
@@ -11,7 +11,7 @@ package com.yf.annotationdemo.annotation_define;
 /**
  * 此处展示给class类AnnotationMemberUsage使用AnnotationTest注解，并给注解的成员变量赋值；
  * 如果注解部分或者全部的成员变量设置了默认值，在使用该注解时候可以不给成员变量赋值，
- * 就是括号部分不要了
+ * 就是括号部分不要了，如果注解没有任何成员变量的时候，使用该注解的时候，也可以把括号去掉；
  */
 @AnnotationTest(name = "alv_chi",age = 25)
 public class AnnotationMemberUsage {
