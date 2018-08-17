daaf61cf11ef01bfb427c3ee4a0f0e0f8697a454
zhizhongbiao <zhizhongbiao163@163.com>
2018-08-17 09:51:44 +0800

    add-增加AnnotationMemberUsage类，展示注解的成员变量的赋值 update-更新一下AnnotationTest类，展示注解定义成员变量并给成员变量默认值

---
+++ app/src/main/java/com/yf/annotationdemo/annotation_define/AnnotationMemberUsage.java
@@ -0,0 +1,16 @@
+package com.yf.annotationdemo.annotation_define;
+
+/**
+ * FileName :  AnnotationMemberUsage
+ * Author   :  zhizhongbiao
+ * Date     :  2018/8/17
+ * Describe :
+ */
+
+
+/**
+ * 此处展示给class类AnnotationMemberUsage使用AnnotationTest注解，并给注解的成员变量赋值；
+ */
+@AnnotationTest(name = "alv_chi",age = 25)
+public class AnnotationMemberUsage {
+}
--- app/src/main/java/com/yf/annotationdemo/annotation_define/AnnotationTest.java
+++ app/src/main/java/com/yf/annotationdemo/annotation_define/AnnotationTest.java
@@ -12,4 +12,20 @@ package com.yf.annotationdemo.annotation_define;
 public @interface AnnotationTest {
     //这里声明了一个名为Annotation的注解
     //注解有点像接口一样的声明方式，只不过要在interface前面加一个@标志符号
+
+
+    /**
+     * 注解只有成员变量，没有方法，并且成员变量的声明
+     * 与class类的成员变量有一点点不同就是变量后面要加括号，
+     * 变量前面还是变量类型,并且都是public的，
+     * 设置成员变量默认值的时候，和class类不同，
+     * 注解的成员变量设置默认值时候要用到default关键字，位置是在class中“=”的位置
+     */
+
+    //没默认值得成员变量
+    String name();
+
+    //有默认值的变量
+    int age() default 24;
+
 }
